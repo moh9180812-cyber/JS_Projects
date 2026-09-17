5b1fc02bfff4c15858ec4ed78bf7a420d364ed65
Mohamed Osama <mo5890601@gmail.com>
2026-09-17 18:30:12 +0300

    this files of week 16 .

---
+++ Assignments/Week_16/#06/main.js
@@ -0,0 +1,69 @@
+let chosen = 1;
+
+let myFriends = [
+  { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+  { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+  { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
+
+// Write Your Code Here
+let [one,two,three] = myFriends;
+
+let {title: name1,age: age1,available: av1,skills: [,a1] } = one;
+let {title: name2,age: age2,available: av2,skills: [,a2] } = two;
+let {title: name3,age: age3,available: av3,skills: [,a3] } = three;
+
+
+if (chosen === 1) {
+  console.log(name1)
+  console.log(age1)
+  if (av1 === true) {
+    console.log(`Available`);
+  }
+  else if (av1 === false) {
+    console.log(`Not Available`);
+  }
+  console.log(a1);
+}
+else if (chosen === 2) {
+  console.log(name2)
+  console.log(age2)
+  if (av2 === true) {
+    console.log(`Available`);
+  }
+  else if (av2 === false) {
+    console.log(`Not Available`);
+  }
+  console.log(a2);
+}
+else if (chosen === 3) {
+  console.log(name3)
+  console.log(age3)
+  if (av3 === true) {
+    console.log(`Available`);
+  }
+  else if (av3 === false) {
+    console.log(`Not Available`);
+  }
+  console.log(a3);
+}
+// If chosen === 1
+
+"Osama"
+39
+"Available"
+"CSS"
+
+// If chosen === 2
+
+"Ahmed"
+25
+"Not Available"
+"Django"
+
+// If chosen === 3
+
+"Sayed"
+33
+"Available"
+"Laravel"
--- Lessons/Week_15/#114/main.js
+++ Lessons/Week_15/#114/main.js
@@ -1,10 +1,10 @@
 // style of body .
-document.body.style = "display: flex; justify-content: center; flex-direction: column";
+document.body.style = "display: flex; justify-content: center; flex-direction: column; width: 100%";
 
 // create form container and style it .
 let form = document.createElement("div");
 form.className = "form";
-form.style = "background-color: #ddd; margin-top: 20px; padding: 20px; border-radius: 10px; display: flex; justify-content: space-between; gap: 20px";
+form.style = "background-color: #ddd; width: 500px;margin-left: 50%; transform: translateX(-50%); margin-top: 20px; padding: 20px; border-radius: 10px; display: flex; justify-content: space-between; gap: 20px";
 
 // create input and style it .
 let taskInp = document.createElement("input");
@@ -22,3 +22,91 @@ form.append(addBtn);
 
 // append form in body .
 document.body.append(form);
+
+
+
+
+let boxTasks = document.createElement("div");
+  boxTasks.style = "background-color: #ddd; width: 500px;margin-left: 50%; transform: translateX(-50%); margin-top: 20px; padding: 20px; border-radius: 10px; display: flex; justify-content: space-between; flex-direction: column; gap: 10px";
+
+
+ let arrValue = window.localStorage.tasks
+  ? window.localStorage.tasks.split(",")
+  : [];
+
+addBtn.onclick = function () {
+  if (taskInp.value === "") {
+    return;      
+  }
+
+  let task = document.createElement("div");
+  task.style = "padding: 10px; border-radius: 10px; display: flex; justify-content: space-between; align-items: center; background-color: white";
+
+  let taskText = document.createElement("span");
+
+  let delBtn = document.createElement("button");
+  delBtn.innerHTML = "Delete";
+  delBtn.style = "padding: 10px; background-color: red; color: white; font-size: 14px; border-radius: 10px; cursor:pointer"
+
+  taskText.innerHTML = taskInp.value;  
+
+  arrValue.push(taskInp.value)
+
+  window.localStorage.setItem("tasks", arrValue);
+  
+  task.append(taskText);
+  task.append(delBtn);
+  boxTasks.append(task);
+
+  delBtn.onclick = function () {
+    task.remove();
+     let index = arrValue.indexOf(taskText.innerHTML);
+
+    arrValue.splice(index, 1);
+
+    window.localStorage.setItem("tasks", arrValue);
+
+    if (boxTasks.children.length === 0) {
+       boxTasks.remove()
+    }
+  }
+
+  taskInp.value = "";
+  document.body.append(boxTasks);
+  
+}
+
+
+for (let i = 0; i < arrValue.length; i++) {
+    
+    let task = document.createElement("div");
+    task.style = "padding: 10px; border-radius: 10px; display: flex; justify-content: space-between; align-items: center; background-color: white";
+
+    let taskText = document.createElement("span");
+    taskText.innerHTML = arrValue[i];
+
+    let delBtn = document.createElement("button");
+    delBtn.innerHTML = "Delete";
+    delBtn.style = "padding: 10px; background-color: red; color: white; font-size: 14px; border-radius: 10px; cursor:pointer";
+
+    task.append(taskText);
+    task.append(delBtn);
+    boxTasks.append(task);
+
+    delBtn.onclick = function () {
+        task.remove();
+
+        let index = arrValue.indexOf(taskText.innerHTML);
+
+        if (index !== -1) {
+            arrValue.splice(index, 1);
+        }
+
+        if (boxTasks.children.length === 0) {
+            boxTasks.remove();
+        }
+
+        window.localStorage.setItem("tasks", arrValue);
+    };
+    document.body.append(boxTasks);
+}
